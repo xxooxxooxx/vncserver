a52f99023cbdd95573982b2a6a2018cbbf6d1e28
xxooxxooxx <panchunmei30@gmail.com>
2018-08-22 13:34:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #/bin/sh
-
 apt-get update
 apt-get  install firefox-esr xfce4 xfce4-goodies gnome-icon-theme tightvncserver -y
 cd /etc/init.d/
 wget https://raw.githubusercontent.com/xxooxxooxx/vncserver/master/vncserver && chmod +x vncserver
-
 systemctl daemon-reload
 systemctl enable vncserver
